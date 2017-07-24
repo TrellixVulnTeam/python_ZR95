7289da88a75380a58bbde22e864122181eaf1d9c
Muhammad Taha <muhammad.taha@novigi.com.au>
2017-07-24 19:03:46 +0500

    my django project

---
+++ Django work/djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\java1\desktop\python\django work\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
